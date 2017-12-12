2154664bd7a6457e26e3c91c5622a84566af02f0
jstansfield0 <stansfieldjc@vcu.edu>
2017-12-12 21:59:52 +0000

    WIP pval adjustment

---
+++ R/adjust_pval.R
@@ -0,0 +1,8 @@
+# function to adjust p-values based on distance holm method
+adjust_pval <- function(hic.table, theta, alpha = 0.05) {
+  # get distance percentages
+  d_percent <- ((hic.table$D + 1) / max(hic.table$D + 1)) * 100
+  # calculate threshold for checking if p-value is < thresh to be a rejection of the null hypothesis
+  threshhold <- alpha / (d_percent * theta)
+  
+} 
